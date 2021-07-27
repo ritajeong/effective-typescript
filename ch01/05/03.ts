b12b7793664a4b22856b32747cde83bdf91dd7eb
ritajeong <litch0s2@gmail.com>
2021-07-27 22:26:16 +0900

    Update files-study day1

--- ch01/05/03.ts
+++ ch01/05/03.ts
@@ -1,14 +1,15 @@
-let age: number
-age = '12'
+// ## any는 함수 시그니처를 무시해 버립니다.
+let age: number;
+age = "12";
 // ~~~ Type '"12"' is not assignable to type 'number'
-age = '12' as any // OK
+age = "12" as any; // OK
 function calculateAge(birthDate: Date): number {
   // COMPRESS
-  return 0
+  return 0;
   // END
 }
 
-let birthDate: any = '1990-01-19'
-calculateAge(birthDate) // OK
+let birthDate: any = "1990-01-19";
+calculateAge(birthDate); // OK
 
-export default {}
+export default {};
